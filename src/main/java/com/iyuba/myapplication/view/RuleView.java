01bc49fe3da026e488ec398daf94781f0c43b516
dumingyu <Howard9891@163.com>
2016-09-23 20:12:02 +0800

    init commit

---
+++ src/main/java/com/iyuba/myapplication/view/RuleView.java
@@ -0,0 +1,21 @@
+package com.iyuba.myapplication.view;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+/**
+ * Created by mingyu on 16-9-23.
+ */
+public class RuleView extends View{
+    public RuleView(Context context) {
+        super(context);
+    }
+    public RuleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public RuleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
